4539a58b0533294cff871e15f0236915562dbcab
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-06-23 17:17:25 +1000

    refactor: Aesthetic

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -3,23 +3,23 @@ import '../styles/tech.scss'
 export default function() {
   return (<>
   <div class='flex flex-col justify-start items-start mt-10 max-w-3xl'>
-    <p>I'm a Full-Stack Developer, though I mostly work in the backend, I mostly use languages such as Rust, Elixir, Svelte, and more!</p>
-    <p>I don't work on large scale projects (Long-term)</p>
+    <p>I'm a Full-Stack Developer, though I mostly work in the backend, I use languages such as Rust, Elixir, Svelte, and more!</p>
+    <p>I don't have much motivation to work on majorly complex projects, one day I will <span>{':)'}</span></p>
     <p>Currently I'm looking around Networking.</p>
 
     <p>Experience:</p>
     <ul>
       <li>
-        <p>React: Intermediate</p>
+        <p>React: <span>~</span> 1 - 2 Years</p>
       </li>
       <li>
-        <p>Svelte: Intermediate</p>
+        <p>Svelte: <span>~</span> 6 Months</p>
       </li>
       <li>
-        <p>Rust: Intermediate</p>
+        <p>Rust: 1<span>+</span> Years</p>
       </li>
       <li>
-        <p>Elixir: Intermediate</p>
+        <p>Elixir: <span>~</span> 1 Year</p>
       </li>
     </ul>
 
